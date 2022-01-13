7d63cddc1d94049f79ebb088976cec27f1bec0ec
Syahrul <syahrulram12@gmail.com>
2022-01-13 18:20:23 +0700

    asd

--- lib/models/belajar/AudioPlayer.dart
+++ lib/models/belajar/AudioPlayer.dart
@@ -1,36 +1,39 @@
-class AudioPlayerModel extends Equatable {
+// import 'package:audioplayers/audioplayers.dart';
+// import 'package:equatable/equatable.dart';
 
-  final String id;
-  final Audio audio;
-  final bool isPlaying;
+// class AudioPlayerModel extends Equatable {
+//   final String id;
+//   final Audio audio;
+//   final bool isPlaying;
 
-  const AudioPlayerModel({this.id, this.audio, this.isPlaying});
+//   const AudioPlayerModel(
+//       {required this.id, this.audio, required this.isPlaying});
 
-  @override
-  List<Object> get props => [this.id, this.isPlaying];
+//   @override
+//   List<Object> get props => [this.id, this.isPlaying];
 
-  @override
-  bool get stringify => true;
-}
+//   @override
+//   bool get stringify => true;
+// }
 
-abstract class AudioPlayerRepository {
-  Future<AudioPlayerModel> getById(String audioPlayerId);
-  Future<List<AudioPlayerModel>> getAll();
-}
+// abstract class AudioPlayerRepository {
+//   Future<AudioPlayerModel> getById(String audioPlayerId);
+//   Future<List<AudioPlayerModel>> getAll();
+// }
 
-class InMemoryAudioPlayerRepository implements AudioPlayerRepository {
+// class InMemoryAudioPlayerRepository implements AudioPlayerRepository {
+//   final List<AudioPlayerModel> audioPlayerModels;
 
-  final List<AudioPlayerModel> audioPlayerModels;
+//   InMemoryAudioPlayerRepository({required this.audioPlayerModels});
 
-  InMemoryAudioPlayerRepository({this.audioPlayerModels});
+//   @override
+//   Future<AudioPlayerModel> getById(String audioPlayerId) async {
+//     return Future.value(
+//         audioPlayerModels.firstWhere((model) => model.id == audioPlayerId));
+//   }
 
-  @override
-  Future<AudioPlayerModel> getById(String audioPlayerId) async {
-    return Future.value(audioPlayerModels.firstWhere((model) => model.id == audioPlayerId));
-  }
-
-  @override
-  Future<List<AudioPlayerModel>> getAll() async {
-    return Future.value(audioPlayerModels);
-  }
-}
+//   @override
+//   Future<List<AudioPlayerModel>> getAll() async {
+//     return Future.value(audioPlayerModels);
+//   }
+// }
