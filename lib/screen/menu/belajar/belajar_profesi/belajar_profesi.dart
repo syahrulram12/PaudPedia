d7711d5257db79bef52a65667ddeea8c42b45377
mayaseptaw <maia.septawijaya0409@gmail.com>
2022-01-24 06:38:21 -0800

    profesi

--- lib/screen/menu/belajar/belajar_profesi/belajar_profesi.dart
+++ lib/screen/menu/belajar/belajar_profesi/belajar_profesi.dart
@@ -7,8 +7,22 @@ import 'package:paudpedia_kemendikbud/styles/color.dart';
 import 'package:paudpedia_kemendikbud/screen/menu/belajar/belajar_profesi/profesi_detail.dart';
 import 'package:paudpedia_kemendikbud/screen/menu/belajar/belajar_profesi/belajar_profesi.dart';
 
-class ProfesiHome extends StatelessWidget {
+class ProfesiHome extends StatefulWidget {
   const ProfesiHome({Key? key}) : super(key: key);
+
+  @override
+  _ProfesiHomeState createState() => _ProfesiHomeState();
+}
+
+class _ProfesiHomeState extends State<ProfesiHome> {
+  AudioCache SfxProfesi = AudioCache();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SfxProfesi.play('audio/BELAJAR/profesi/belajar-profesi.mp3');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,82 +31,95 @@ class ProfesiHome extends StatelessWidget {
 
     return Scaffold(
         body: Stack(
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(
-                        'assets/images/all_background/belajar/10_belajar_profesi-01.jpg'),
-                    fit: BoxFit.cover),
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(
+                    'assets/images/all_background/belajar/10_belajar_profesi-01.jpg'),
+                fit: BoxFit.cover),
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Container(
+                        child: ClipRRect(
+                          child: GestureDetector(
+                              onTap: () => {Get.back()},
+                              child: Image.asset(
+                                'assets/icons/tab_bar_menu.png',
+                                width: 100,
+                              )),
+                        ),
+                      ),
+                      Container(
+                        child: ClipRRect(
+                          child: GestureDetector(
+                              child: Image.asset(
+                            'assets/icons/tab_bar_auto.png',
+                            width: 125,
+                          )),
+                        ),
+                      ),
+                    ]),
               ),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    child: Row(  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Container(
-                            child: ClipRRect(
-                              child: GestureDetector(
-                                  onTap: () => {Get.back()},
-                                  child: Image.asset(
-                                    'assets/icons/tab_bar_menu.png',
-                                    width: 100,
-                                  )),
-                            ),
-                          ),
-                          Container(
-                            child: ClipRRect(
-                              child: GestureDetector(
-                                  child: Image.asset(
-                                    'assets/icons/tab_bar_auto.png',
-                                    width: 125,
-                                  )),
-                            ),
-                          ),
-                        ]),
-                  ),
-                  SizedBox(height: height / 10),
-                  Container(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 1),
-                        child: Profesi1(),
-                      ))
-                ],
-              ),
-            ),
-          ],
-        ));
+              SizedBox(height: height / 10),
+              Container(
+                  child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 1),
+                child: Profesi1(),
+              ))
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
 
-class ProfesiItem extends StatelessWidget {
+class ProfesiItem extends StatefulWidget {
   final String Imageurl;
+  final String audioUrl;
   final Function()? OnMenuClick;
-
-  const ProfesiItem({Key? key, required this.Imageurl, this.OnMenuClick})
+  const ProfesiItem(
+      {Key? key,
+      required this.Imageurl,
+      required this.audioUrl,
+      this.OnMenuClick})
       : super(key: key);
+
+  @override
+  _ProfesiItemState createState() => _ProfesiItemState();
+}
+
+class _ProfesiItemState extends State<ProfesiItem> {
+  AudioCache SfxProfesi = AudioCache();
 
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Container(
-          margin: EdgeInsets.symmetric(vertical: 6, horizontal: 3),
-          width: 150,
-          height: 80,
-          child: ClipRRect(
-            child: GestureDetector(
-              onTap: OnMenuClick,
-              child: Image.asset(
-                'assets/icons/' + Imageurl + '',
-                fit: BoxFit.contain,
-                    height: 80,
-                    width: 150,
-                  ),
-              ),
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 6, horizontal: 3),
+        width: 150,
+        height: 80,
+        child: ClipRRect(
+          child: GestureDetector(
+            onTap: () {
+              SfxProfesi.play('audio/BELAJAR/profesi/' + widget.audioUrl);
+            },
+            child: Image.asset(
+              'assets/icons/' + widget.Imageurl + '',
+              fit: BoxFit.contain,
+              height: 80,
+              width: 150,
             ),
           ),
-        );
-
+        ),
+      ),
+    );
   }
 }
--- lib/screen/menu/belajar/belajar_profesi/profesi_detail.dart
+++ lib/screen/menu/belajar/belajar_profesi/profesi_detail.dart
@@ -6,7 +6,6 @@ import 'package:path_provider/path_provider.dart';
 import 'package:paudpedia_kemendikbud/styles/color.dart';
 import 'package:paudpedia_kemendikbud/screen/menu/belajar/belajar_profesi/belajar_profesi.dart';
 
-
 class Profesi1 extends StatelessWidget {
   const Profesi1({Key? key}) : super(key: key);
 
@@ -15,53 +14,62 @@ class Profesi1 extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
     return Container(
         child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Flexible(
-                      child: ProfesiItem(
-                          Imageurl: 'icon_profesi/pilih_profesi.png',
-                          
-                      ))
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Flexible(
-                      child: ProfesiItem(
-                          Imageurl: 'icon_profesi/icon_teacher.png')),
-                  Flexible(
-                      child: ProfesiItem(
-                          Imageurl: 'icon_profesi/icon_fire.png')),
-    ],
-    ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Flexible(
-                      child: ProfesiItem(
-                          Imageurl: 'icon_profesi/icon_doctor.png')),
-                  Flexible(
-                      child: ProfesiItem(
-                          Imageurl: 'icon_profesi/icon_police.png')),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Flexible(
-                      child: ProfesiItem(
-                          Imageurl: 'icon_profesi/icon_waiter.png')),
-                  Flexible(
-                      child: Container()),
-                ],
-              ),
-    ],
-        ));
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Flexible(
+                child: ProfesiItem(
+              Imageurl: 'icon_profesi/pilih_profesi.png',
+              audioUrl: 'belajar-mengenal-profesi.mp3',
+            )),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Flexible(
+                child: ProfesiItem(
+              Imageurl: 'icon_profesi/icon_teacher.png',
+              audioUrl: 'guru.mp3',
+            )),
+            Flexible(
+                child: ProfesiItem(
+              Imageurl: 'icon_profesi/icon_fire.png',
+              audioUrl: 'pemadam-kebakaran.mp3',
+            )),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Flexible(
+                child: ProfesiItem(
+              Imageurl: 'icon_profesi/icon_doctor.png',
+              audioUrl: 'dokter.mp3',
+            )),
+            Flexible(
+                child: ProfesiItem(
+              Imageurl: 'icon_profesi/icon_police.png',
+              audioUrl: 'polisi.mp3',
+            )),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Flexible(
+                child: ProfesiItem(
+              Imageurl: 'icon_profesi/icon_waiter.png',
+              audioUrl: 'pelayan.mp3',
+            )),
+            Flexible(child: Container()),
+          ],
+        ),
+      ],
+    ));
   }
 }
