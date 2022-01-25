06084cd1525c92e44d09fe76949cec87ce864b8f
mayaseptaw <maia.septawijaya0409@gmail.com>
2022-01-25 07:30:48 -0800

    hewan

--- lib/screen/menu/belajar/belajar_hewan/belajar_hewan.dart
+++ lib/screen/menu/belajar/belajar_hewan/belajar_hewan.dart
@@ -4,6 +4,8 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:get/get.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:paudpedia_kemendikbud/styles/color.dart';
+import 'package:paudpedia_kemendikbud/screen/menu/belajar/belajar_hewan/hewan_detail.dart';
+import 'package:paudpedia_kemendikbud/screen/menu/belajar/belajar_hewan/belajar_hewan.dart';
 
 class HewanHome extends StatefulWidget {
   const HewanHome({Key? key}) : super(key: key);
@@ -13,106 +15,167 @@ class HewanHome extends StatefulWidget {
 }
 
 class _HewanHomeState extends State<HewanHome> {
-  @override
-  double Benda = 1;
-  String ImageBg =
-      'assets/images/all_background/belajar/09_belajar_hewan_background_hutan.jpg';
+  AudioCache SfxHewan = AudioCache();
+  int page = 1;
 
-  ChangeStyle(Benda) {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SfxHewan.play('audio/BELAJAR/hewan/belajar-fauna.mp3');
+  }
+
+  ChangePage(act) {
     setState(() {
-      if (Benda == 1) {
-        ImageBg =
-            'assets/images/all_background/belajar/09_belajar_hewan_background_hutan.jpg';
-      } else if (Benda == 2) {
-        ImageBg =
-            'assets/images/all_background/belajar/09_belajar_hewan_background_ikanhias.jpg';
-      } else if (Benda == 3) {
-        ImageBg =
-            'assets/images/all_background/belajar/09_belajar_hewan_background_langka.jpg';
+      if (act == 'tambah') {
+        page = page + 1;
+        if (page > 4) {
+          page = 4;
+        }
       } else {
-        ImageBg =
-            'assets/images/all_background/belajar/09_belajar_hewan_background_laut.jpg';
+        page = page - 1;
+        if (page < 1) {
+          page = 1;
+        }
       }
     });
+  }
+
+  Page() {
+    if (page == 1) {
+      return Page1();
+    } else if (page == 2) {
+      return Page2();
+    } else if (page == 3) {
+      return Page3();
+    } else {
+      return Page4();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
-        body: Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(image: AssetImage(ImageBg))),
-            child: Column(children: [
+        body: Stack(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(
+                    'assets/images/all_background/belajar/09_belajar_hewan_background_hutan.jpg'),
+                fit: BoxFit.cover),
+          ),
+          child: Column(
+            children: <Widget>[
               Container(
-                  color: Colors.white,
-                  child: Row(
+                margin: EdgeInsets.symmetric(
+                    horizontal: (width / 8) / 2, vertical: 25),
+                height: (height / 8) / 2,
+                child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     mainAxisSize: MainAxisSize.max,
                     children: [
                       Container(
+                        width: width / 6,
                         child: ClipRRect(
                           child: GestureDetector(
                               onTap: () => {Get.back()},
                               child: Image.asset(
-                                'assets/icons/tab_bar_menu.png',
-                                width: 100,
-                              )),
+                                  'assets/icons/tab_bar_menu.png',
+                                  fit: BoxFit.contain)),
                         ),
                       ),
                       Container(
-                        child: ClipRRect(
-                          child: GestureDetector(
-                              onTap: () {
-                                ChangeStyle(3);
-                              },
-                              child: Image.asset(
-                                'assets/icons/tab_bar_auto.png',
-                                width: 125,
-                              )),
+                        width: width / 5,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage(
+                                    'assets/icons/tab_bar_right_left.png'),
+                                fit: BoxFit.contain)),
+                        child: Row(
+                          children: [
+                            Flexible(
+                              child: Container(
+                                child: GestureDetector(
+                                  onTap: () {
+                                    ChangePage('kurang');
+                                  },
+                                ),
+                              ),
+                            ),
+                            Flexible(child: Container(
+                              child: GestureDetector(
+                                onTap: () {
+                                  ChangePage('tambah');
+                                },
+                              ),
+                            ))
+                          ],
                         ),
                       ),
-                    ],
-                  )),
-              // Container(
-              //   color: Colors.white,
-              //   width: 100,
-              //   child: Stack(
-              //     children: [
-              //       Positioned(
-              //           top: 15,
-              //           child: Column(
-              //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //             mainAxisSize: MainAxisSize.max,
-              //             children: [
-              //               Container(
-              //                 child: ClipRRect(
-              //                   child: GestureDetector(
-              //                       onTap: () => {Get.back()},
-              //                       child: Image.asset(
-              //                         'assets/icons/tab_bar_menu.png',
-              //                         width: 100,
-              //                       )),
-              //                 ),
-              //               ),
-              //               Container(
-              //                 child: ClipRRect(
-              //                   child: GestureDetector(
-              //                       onTap: () {
-              //                         ChangeStyle(3);
-              //                       },
-              //                       child: Image.asset(
-              //                         'assets/icons/tab_bar_auto.png',
-              //                         width: 125,
-              //                       )),
-              //                 ),
-              //               ),
-              //             ],
-              //           )),
-              //     ],
-              //   ),
-              // )
-            ])));
+                      Container(
+                        width: width / 5,
+                        child: ClipRRect(
+                          child: GestureDetector(
+                              child: Image.asset(
+                            'assets/icons/tab_bar_auto.png',
+                            width: 125,
+                          )),
+                        ),
+                      ),
+                    ]),
+              ),
+              SizedBox(height: height / 10),
+              Container(
+                  child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 2),
+                child: Page(),
+              ))
+            ],
+          ),
+        ),
+      ],
+    ));
+  }
+}
+
+class HewanItem extends StatefulWidget {
+  final String Imageurl;
+  final String audioUrl;
+  final Function()? OnMenuClick;
+  const HewanItem(
+      {Key? key,
+      required this.Imageurl,
+      this.OnMenuClick,
+      required this.audioUrl})
+      : super(key: key);
+
+  @override
+  _HewanItemState createState() => _HewanItemState();
+}
+
+class _HewanItemState extends State<HewanItem> {
+  AudioCache SfxHewan = AudioCache();
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(13),
+        child: GestureDetector(
+          onTap: () {
+            SfxHewan.play('audio/BELAJAR/fauna/' + widget.audioUrl);
+          },
+          child: Image.asset(
+            'assets/icons/' + widget.Imageurl + '',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    ));
+    ;
   }
 }
--- lib/screen/menu/belajar/belajar_warna/warna_detail.dart
+++ lib/screen/menu/belajar/belajar_warna/warna_detail.dart
@@ -12,42 +12,18 @@ class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(children: <Widget>[
-      Container(
-        margin: EdgeInsets.symmetric(horizontal: 8 / 2, vertical: 25),
-        height: 8 / 2,
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Container(
-                width: 6,
-                child: ClipRRect(
-                  child: GestureDetector(
-                      onTap: () => {Get.back()},
-                      child: Image.asset('assets/icons/tab_bar_kanan.png',
-                          fit: BoxFit.contain)),
-                ),
-              ),
-              Flexible(
-                  child: WarnaItem(
-                Imageurl: 'icon_cake/icon_cake-01-merah.png',
-                audioUrl: 'merah.mp3',
-              )),
-              Flexible(
-                child: Container(
-                  width: 5,
-                  child: ClipRRect(
-                    child: GestureDetector(
-                        child: Image.asset(
-                      'assets/icons/tab_bar_kiri.png',
-                      width: 125,
-                    )),
-                  ),
-                ),
-              )
-            ]),
-      )
-    ]));
+        child: Column(
+      children: [
+        Row(
+          children: [
+            Flexible(
+                child: WarnaItem(
+              Imageurl: 'icon_number/icon_1-10/icon_1-10-01.png',
+              audioUrl: '1.mp3',
+            )),
+          ],
+        ),
+      ],
+    ));
   }
 }
