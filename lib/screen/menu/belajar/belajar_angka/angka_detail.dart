2d73fa5fa6fde57f9f6b307a9ce93706607159c9
Syahrul <syahrulram12@gmail.com>
2022-03-30 20:08:00 +0700

    asd

--- lib/screen/menu/belajar/belajar_angka/angka_detail.dart
+++ lib/screen/menu/belajar/belajar_angka/angka_detail.dart
@@ -15,28 +15,17 @@ class Page1 extends StatefulWidget {
   _Page1State createState() => _Page1State();
 }
 
-var AngkaItem1 = 0.5;
-var AngkaItem2 = 0.5;
-var AngkaItem3 = 0.5;
-var AngkaItem4 = 0.5;
-var AngkaItem5 = 0.5;
-var AngkaItem6 = 0.5;
-var AngkaItem7 = 0.5;
-var AngkaItem8 = 0.5;
-var AngkaItem9 = 0.5;
-var AngkaItem10 = 0.5;
-
 class _Page1State extends State<Page1> with TickerProviderStateMixin {
-  var AngkaItem1 = 0.5;
-  var AngkaItem2 = 0.5;
-  var AngkaItem3 = 0.5;
-  var AngkaItem4 = 0.5;
-  var AngkaItem5 = 0.5;
-  var AngkaItem6 = 0.5;
-  var AngkaItem7 = 0.5;
-  var AngkaItem8 = 0.5;
-  var AngkaItem9 = 0.5;
-  var AngkaItem10 = 0.5;
+  var AngkaItem1 = 1;
+  var AngkaItem2 = 1;
+  var AngkaItem3 = 1;
+  var AngkaItem4 = 1;
+  var AngkaItem5 = 1;
+  var AngkaItem6 = 1;
+  var AngkaItem7 = 1;
+  var AngkaItem8 = 1;
+  var AngkaItem9 = 1;
+  var AngkaItem10 = 1;
   late AnimationController _controller1;
   late AnimationController _controller2;
   late AnimationController _controller3;
@@ -52,7 +41,7 @@ class _Page1State extends State<Page1> with TickerProviderStateMixin {
   void initState() {
     _controller1 = AnimationController(
         vsync: this,
-        lowerBound: 0.5,
+        lowerBound: 1,
         upperBound: 1.0,
         duration: Duration(milliseconds: 300));
     _controller1.addListener(() {
@@ -62,7 +51,7 @@ class _Page1State extends State<Page1> with TickerProviderStateMixin {
     });
     _controller2 = AnimationController(
         vsync: this,
-        lowerBound: 0.5,
+        lowerBound: 1,
         upperBound: 1.0,
         duration: Duration(seconds: 1));
     _controller2.addListener(() {
@@ -72,7 +61,7 @@ class _Page1State extends State<Page1> with TickerProviderStateMixin {
     });
     _controller3 = AnimationController(
         vsync: this,
-        lowerBound: 0.5,
+        lowerBound: 1,
         upperBound: 1.0,
         duration: Duration(milliseconds: 300));
     _controller3.addListener(() {
@@ -82,7 +71,7 @@ class _Page1State extends State<Page1> with TickerProviderStateMixin {
     });
     _controller4 = AnimationController(
         vsync: this,
-        lowerBound: 0.5,
+        lowerBound: 1,
         upperBound: 1.0,
         duration: Duration(seconds: 1));
     _controller4.addListener(() {
@@ -92,7 +81,7 @@ class _Page1State extends State<Page1> with TickerProviderStateMixin {
     });
     _controller5 = AnimationController(
         vsync: this,
-        lowerBound: 0.5,
+        lowerBound: 1,
         upperBound: 1.0,
         duration: Duration(milliseconds: 300));
     _controller5.addListener(() {
@@ -102,7 +91,7 @@ class _Page1State extends State<Page1> with TickerProviderStateMixin {
     });
     _controller6 = AnimationController(
         vsync: this,
-        lowerBound: 0.5,
+        lowerBound: 1,
         upperBound: 1.0,
         duration: Duration(seconds: 1));
     _controller6.addListener(() {
@@ -112,7 +101,7 @@ class _Page1State extends State<Page1> with TickerProviderStateMixin {
     });
     _controller7 = AnimationController(
         vsync: this,
-        lowerBound: 0.5,
+        lowerBound: 1,
         upperBound: 1.0,
         duration: Duration(milliseconds: 300));
     _controller7.addListener(() {
@@ -122,7 +111,7 @@ class _Page1State extends State<Page1> with TickerProviderStateMixin {
     });
     _controller8 = AnimationController(
         vsync: this,
-        lowerBound: 0.5,
+        lowerBound: 1,
         upperBound: 1.0,
         duration: Duration(seconds: 1));
     _controller8.addListener(() {
@@ -132,7 +121,7 @@ class _Page1State extends State<Page1> with TickerProviderStateMixin {
     });
     _controller9 = AnimationController(
         vsync: this,
-        lowerBound: 0.5,
+        lowerBound: 1,
         upperBound: 1.0,
         duration: Duration(milliseconds: 300));
     _controller9.addListener(() {
@@ -142,7 +131,7 @@ class _Page1State extends State<Page1> with TickerProviderStateMixin {
     });
     _controller10 = AnimationController(
         vsync: this,
-        lowerBound: 0.5,
+        lowerBound: 1,
         upperBound: 1.0,
         duration: Duration(seconds: 1));
     _controller10.addListener(() {
@@ -262,6 +251,382 @@ class _Page1State extends State<Page1> with TickerProviderStateMixin {
 //   }
 // }
 
+class Page2 extends StatefulWidget {
+  const Page2({ Key? key }) : super(key: key);
+
+  @override
+  _Page2State createState() => _Page2State();
+}
+
+class _Page2State extends State<Page2> {
+   var AngkaItem1 = 1;
+  var AngkaItem2 = 1;
+  var AngkaItem3 = 1;
+  var AngkaItem4 = 1;
+  var AngkaItem5 = 1;
+  var AngkaItem6 = 1;
+  var AngkaItem7 = 1;
+  var AngkaItem8 = 1;
+  var AngkaItem9 = 1;
+  var AngkaItem10 = 1;
+  late AnimationController _controller1;
+  late AnimationController _controller2;
+  late AnimationController _controller3;
+  late AnimationController _controller4;
+  late AnimationController _controller5;
+  late AnimationController _controller6;
+  late AnimationController _controller7;
+  late AnimationController _controller8;
+  late AnimationController _controller9;
+  late AnimationController _controller10;
+
+  @override
+  void initState() {
+    _controller1 = AnimationController(
+        vsync: this,
+        lowerBound: 1,
+        upperBound: 1.0,
+        duration: Duration(milliseconds: 300));
+    _controller1.addListener(() {
+      setState(() {
+        AngkaItem1 = _controller1.value;
+      });
+    });
+    _controller2 = AnimationController(
+        vsync: this,
+        lowerBound: 1,
+        upperBound: 1.0,
+        duration: Duration(seconds: 1));
+    _controller2.addListener(() {
+      setState(() {
+        AngkaItem2 = _controller2.value;
+      });
+    });
+    _controller3 = AnimationController(
+        vsync: this,
+        lowerBound: 1,
+        upperBound: 1.0,
+        duration: Duration(milliseconds: 300));
+    _controller3.addListener(() {
+      setState(() {
+        AngkaItem3 = _controller3.value;
+      });
+    });
+    _controller4 = AnimationController(
+        vsync: this,
+        lowerBound: 1,
+        upperBound: 1.0,
+        duration: Duration(seconds: 1));
+    _controller4.addListener(() {
+      setState(() {
+        AngkaItem4 = _controller4.value;
+      });
+    });
+    _controller5 = AnimationController(
+        vsync: this,
+        lowerBound: 1,
+        upperBound: 1.0,
+        duration: Duration(milliseconds: 300));
+    _controller5.addListener(() {
+      setState(() {
+        AngkaItem5 = _controller5.value;
+      });
+    });
+    _controller6 = AnimationController(
+        vsync: this,
+        lowerBound: 1,
+        upperBound: 1.0,
+        duration: Duration(seconds: 1));
+    _controller6.addListener(() {
+      setState(() {
+        AngkaItem6 = _controller6.value;
+      });
+    });
+    _controller7 = AnimationController(
+        vsync: this,
+        lowerBound: 1,
+        upperBound: 1.0,
+        duration: Duration(milliseconds: 300));
+    _controller7.addListener(() {
+      setState(() {
+        AngkaItem7 = _controller7.value;
+      });
+    });
+    _controller8 = AnimationController(
+        vsync: this,
+        lowerBound: 1,
+        upperBound: 1.0,
+        duration: Duration(seconds: 1));
+    _controller8.addListener(() {
+      setState(() {
+        AngkaItem8 = _controller8.value;
+      });
+    });
+    _controller9 = AnimationController(
+        vsync: this,
+        lowerBound: 1,
+        upperBound: 1.0,
+        duration: Duration(milliseconds: 300));
+    _controller9.addListener(() {
+      setState(() {
+        AngkaItem9 = _controller9.value;
+      });
+    });
+    _controller10 = AnimationController(
+        vsync: this,
+        lowerBound: 1,
+        upperBound: 1.0,
+        duration: Duration(seconds: 1));
+    _controller10.addListener(() {
+      setState(() {
+        AngkaItem10 = _controller10.value;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller1.dispose();
+    _controller2.dispose();
+    _controller3.dispose();
+    _controller4.dispose();
+    _controller5.dispose();
+    _controller6.dispose();
+    _controller7.dispose();
+    _controller8.dispose();
+    _controller9.dispose();
+    _controller10.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: [
+        Row(
+          children: [
+            Flexible(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _controller1.forward(from: 0.0);
+                  });
+                  Timer(Duration(seconds: 1), () {
+                    setState(() {
+                      _controller1.reverse();
+                    });
+                  });
+                },
+                child: Transform.scale(
+                  scale: AngkaItem1,
+                  child: AngkaItem(
+                    Imageurl: 'icon_number/icon_11-20/icon_11-20-11.png',
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _controller1.forward(from: 0.0);
+                  });
+                  Timer(Duration(seconds: 1), () {
+                    setState(() {
+                      _controller1.reverse();
+                    });
+                  });
+                },
+                child: Transform.scale(
+                  scale: AngkaItem1,
+                  child: AngkaItem(
+                    Imageurl: 'icon_number/icon_11-20/icon_11-20-12.png',
+                  ),
+                ),
+              ),
+            ),
+           Flexible(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _controller1.forward(from: 0.0);
+                  });
+                  Timer(Duration(seconds: 1), () {
+                    setState(() {
+                      _controller1.reverse();
+                    });
+                  });
+                },
+                child: Transform.scale(
+                  scale: AngkaItem1,
+                  child: AngkaItem(
+                    Imageurl: 'icon_number/icon_11-20/icon_11-20-13.png',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Flexible(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _controller1.forward(from: 0.0);
+                  });
+                  Timer(Duration(seconds: 1), () {
+                    setState(() {
+                      _controller1.reverse();
+                    });
+                  });
+                },
+                child: Transform.scale(
+                  scale: AngkaItem1,
+                  child: AngkaItem(
+                    Imageurl: 'icon_number/icon_11-20/icon_11-20-14.png',
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _controller1.forward(from: 0.0);
+                  });
+                  Timer(Duration(seconds: 1), () {
+                    setState(() {
+                      _controller1.reverse();
+                    });
+                  });
+                },
+                child: Transform.scale(
+                  scale: AngkaItem1,
+                  child: AngkaItem(
+                    Imageurl: 'icon_number/icon_11-20/icon_11-20-15.png',
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _controller1.forward(from: 0.0);
+                  });
+                  Timer(Duration(seconds: 1), () {
+                    setState(() {
+                      _controller1.reverse();
+                    });
+                  });
+                },
+                child: Transform.scale(
+                  scale: AngkaItem1,
+                  child: AngkaItem(
+                    Imageurl: 'icon_number/icon_11-20/icon_11-20-16.png',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Flexible(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _controller1.forward(from: 0.0);
+                  });
+                  Timer(Duration(seconds: 1), () {
+                    setState(() {
+                      _controller1.reverse();
+                    });
+                  });
+                },
+                child: Transform.scale(
+                  scale: AngkaItem1,
+                  child: AngkaItem(
+                    Imageurl: 'icon_number/icon_11-20/icon_11-20-17.png',
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _controller1.forward(from: 0.0);
+                  });
+                  Timer(Duration(seconds: 1), () {
+                    setState(() {
+                      _controller1.reverse();
+                    });
+                  });
+                },
+                child: Transform.scale(
+                  scale: AngkaItem1,
+                  child: AngkaItem(
+                    Imageurl: 'icon_number/icon_11-20/icon_11-20-18.png',
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _controller1.forward(from: 0.0);
+                  });
+                  Timer(Duration(seconds: 1), () {
+                    setState(() {
+                      _controller1.reverse();
+                    });
+                  });
+                },
+                child: Transform.scale(
+                  scale: AngkaItem1,
+                  child: AngkaItem(
+                    Imageurl: 'icon_number/icon_11-20/icon_11-20-19.png',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Flexible(child: Container()),
+            Flexible(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _controller1.forward(from: 0.0);
+                  });
+                  Timer(Duration(seconds: 1), () {
+                    setState(() {
+                      _controller1.reverse();
+                    });
+                  });
+                },
+                child: Transform.scale(
+                  scale: AngkaItem1,
+                  child: AngkaItem(
+                    Imageurl: 'icon_number/icon_11-20/icon_11-20-20.png',
+                  ),
+                ),
+              ),
+            ),
+            Flexible(child: Container())
+          ],
+        ),
+      ],
+    ));
+  }
+}
 class Page2 extends StatelessWidget {
   const Page2({Key? key}) : super(key: key);
 
